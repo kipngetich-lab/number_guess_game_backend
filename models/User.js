9f2b2a86762f817700aab1fa8e13820b71fc0489
Kipngetich Josphat <joskipngetich07@gmail.com>
2025-09-07 20:18:33 +0300

    initial commit

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  accumulatedReward: { type: Number, default: 0 },
+  hasAttempted: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model('User', UserSchema);
